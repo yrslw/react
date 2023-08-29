694e9f1e2b29ef3383ba80fe153f4b1f012b8888
yrslw <mikky12111@gmail.com>
2023-08-29 10:03:00 +0700

    a

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const app = require('./app')
 const port = process.env.PORT || 5000
 
 app.listen(port, () => 
-    console.log('server has been rabotaet ${port}')    )
+    console.log(`server has been rabotaet ${port}`)    )
