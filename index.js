2bd2559cda1e019126b70fe3c6b0fc1dcb294f2d
yrslw <mikky12111@gmail.com>
2023-08-29 09:51:13 +0700

    s

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => { 
-    res.status(200).json({
-        message: 'Rabotaet'
-    })
+const app = require('./app')
+const port = process.env.PORT || 5000
 
-})
-app.listen(5000, () => {
-    console.log('server has been rabotaet')
-})
+app.listen(port, () => 
+    console.log('server has been rabotaet ${port}')    )
