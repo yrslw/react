6828bc6d5938eb3cb1c374b5ca22e1745adac5a6
yrslw <mikky12111@gmail.com>
2023-08-30 11:48:45 +0700

    1

---
+++ controllers/auth.js
@@ -0,0 +1,15 @@
+module.exports.login = function (req, res){
+    res.status(200).json({
+        login: 'from controller'
+    })
+}
+
+
+
+
+
+module.exports.register = function (req, res) {
+    res.status(200).json({
+        register: 'from controller' 
+})
+}
